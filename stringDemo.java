e5bf1060bd887a5ec57b75d000c106bd88b3797d
prasiddhineupane <102519290+prasiddhineupane@users.noreply.github.com>
2023-08-07 19:55:30 -0500

    Update stringDemo.java

--- stringDemo.java
+++ stringDemo.java
@@ -1,7 +1,7 @@
 //String is nothing but group or array of characters. It is a pre building class in java.
 //Data Structure is a method which allows to retrieve data easily, and efficiently.
 // String are immutable in nature. Once created can't change it.
-//Two ways to create string. 1. using the new operator
+//Two ways to create string. 1. using the new operator     2. using string literal
 public class stringDemo {
     public static void main(String[] args) {
         String str = "Prasi";
