fa1304d74ad271970ef705acf6ff7606686d0d6d
prasiddhineupane <102519290+prasiddhineupane@users.noreply.github.com>
2023-08-08 21:34:30 -0500

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,71 +1,71 @@
-//
-//    public class Employee extends Object {
-//
-//        int age=22;
-//        String name="Robin";
-//        String designation="Developer";
-//        int salary=1234;
-//
-//        void typing() {
-//            System.out.println("I am a programmer");
-//        }
-//
-//        //creating constructor
-//        //parameterised constructor
-//        Employee(int a, int s){
-//            age=a;
-//            salary=s;
-//        }
-//
-//        //no arg constructor
-//        Employee(){
-//
-//        }
-//
-//        Employee(int a, String n, String d, int s){
-//            age=a;
-//            salary=s;
-//            designation=d;
-//            name=n;
-//
-//
-//        }
-//
-//        public static void main(String[] args) {
-//
-//            Employee ee = new Employee();
-//            Employee ee1 = new Employee();
-//
-//            Employee ee2 = new Employee(24,9876);
-//            Employee ee3 = new Employee(34,"Saurab","tester",567656);
-//
-//
-//            Employee ee4 = new Employee(54,"Alex","techlead",78656);
-//            System.out.println(ee3);
-//            System.out.println(ee);
-//            System.out.println(ee4);
-//
-//            ee.typing();
-//            System.out.println(ee.salary);
-//            System.out.println(ee1.salary);
-//            System.out.println(ee1.age);
-//
-//            System.out.println("!!!!!!!!!!!!!!!!!");
-//            System.out.println(ee2.age);
-//            System.out.println(ee2.salary);
-//
-//        }
-//
-//        @Override
-//        public String toString() {
-//            return "Employee [name=" + name + ", designation=" + designation + "]";
-//        }
-//
-//        //	@Override
-////	public String toString() {
-////		return "Employee [age=" + age + ", name=" + name + ", designation=" + designation + ", salary=" + salary + "]";
-////	}
-//
-//    }
+
+   public class Employee extends Object {
+
+       int age=22;
+       String name="Robin";
+       String designation="Developer";
+       int salary=1234;
+
+       void typing() {
+           System.out.println("I am a programmer");
+       }
+
+       //creating constructor
+       //parameterised constructor
+       Employee(int a, int s){
+           age=a;
+           salary=s;
+       }
+
+       //no arg constructor
+       Employee(){
+
+       }
+
+       Employee(int a, String n, String d, int s){
+           age=a;
+           salary=s;
+           designation=d;
+           name=n;
+
+
+       }
+
+       public static void main(String[] args) {
+
+           Employee ee = new Employee();
+           Employee ee1 = new Employee();
+
+           Employee ee2 = new Employee(24,9876);
+           Employee ee3 = new Employee(34,"Saurab","tester",567656);
+
+
+           Employee ee4 = new Employee(54,"Alex","techlead",78656);
+           System.out.println(ee3);
+           System.out.println(ee);
+           System.out.println(ee4);
+
+           ee.typing();
+           System.out.println(ee.salary);
+           System.out.println(ee1.salary);
+           System.out.println(ee1.age);
+
+           System.out.println("!!!!!!!!!!!!!!!!!");
+           System.out.println(ee2.age);
+           System.out.println(ee2.salary);
+
+       }
+
+       @Override
+       public String toString() {
+           return "Employee [name=" + name + ", designation=" + designation + "]";
+       }
+
+       //	@Override
+//	public String toString() {
+//		return "Employee [age=" + age + ", name=" + name + ", designation=" + designation + ", salary=" + salary + "]";
+//	}
+
+   }
 //}
 
